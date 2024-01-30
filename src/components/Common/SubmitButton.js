e68c11720ce35b3f3f0bb8cb285e8c675d485d13
Sai Krishna Kolli <kollikrishna58@gmail.com>
2024-01-30 10:29:31 -0600

    Added reusable submit button

---
+++ src/components/Common/SubmitButton.js
@@ -0,0 +1,20 @@
+// SubmitButton.js
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../css/Common/SubmitButton.css';
+
+const SubmitButton = ({ title, onSubmit }) => {
+  return (
+    <button className="submit-button" onClick={onSubmit}>
+      {title}
+    </button>
+  );
+};
+
+SubmitButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default SubmitButton;
